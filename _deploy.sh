d7af1332fd2c8c9e5bc9efbd6fa0a13a7b894867
Fernando Corrêa <fptcorrea@gmail.com>
2017-01-26 17:45:29 -0200

    deploy agora remove todo o conteúdo do branch content

--- _deploy.sh
+++ _deploy.sh
@@ -5,11 +5,12 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "dfalbel@gmail.com"
-git config --global user.name "Daniel Falbel"
+git config --global user.email "fptcorrea@gmail.com"
+git config --global user.name "Fernando Corrêa"
 
 git clone -b content https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git out
 cd out
+git rm -rf .
 cp -r ../content/* ./
 git add --all *
 git commit -m "Update the PU" || true
