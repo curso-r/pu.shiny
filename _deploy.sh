3bddf494021691894955d1a43f37fb8157ea91ec
Fernando Corrêa <fptcorrea@gmail.com>
2017-01-26 18:07:31 -0200

    modifica o deploy

--- _deploy.sh
+++ _deploy.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-[ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
-
 git config --global user.email "fptcorrea@gmail.com"
 git config --global user.name "Fernando Corrêa"
 
