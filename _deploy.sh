067f3d37fb909c225a0373c3f069837691a39b7e
Fernando Corrêa <fptcorrea@gmail.com>
2017-01-26 18:25:23 -0200

    adiciona identificação

--- _deploy.sh
+++ _deploy.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+[ -z "${GITHUB_PAT}" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+
 git config --global user.email "fptcorrea@gmail.com"
-git config --global user.name "Fernando Corrêa"
+git config --global user.name "azeloc"
 
 git clone -b content https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git out
 cd out
